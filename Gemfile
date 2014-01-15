35a2deb4220146e84d72e55bad54da2a2ea2a4ee
Jacob Bednarz <jacob.bednarz@gmail.com>
2014-01-15 18:51:29 +1000

    Add base rails app

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+# Rails.
+gem 'rails', '4.0.2'
+
+# Database.
+gem 'pg'
+
+# Assets.
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+
+# Web server.
+gem 'unicorn'
+gem 'foreman'
+
+group :development do
+  gem 'rspec-rails'
+end
+
+# Test dependencies.
+group :development, :test do
+  gem 'better_errors'
+  gem 'dotenv-rails'
+  gem 'pry'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rehttp::Application.config.secret_key_base = 'ca63266c310e0d2311994c9c201ce55b2de821d5d22425d20257b263a4b1196ec9b3985e573fe78d1d940e95f5f857a56f56b278016654934d6c4401b2943128'
