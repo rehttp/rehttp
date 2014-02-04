5f7e349e39158b9f858503b27bc901be0dcc4499
Jacob Bednarz <jacob.bednarz@gmail.com>
2014-02-04 17:52:07 +1000

    Fix CI build settings and initial specs

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -1,17 +1,34 @@
 require 'spec_helper'
+require 'securerandom'
 
-# describe RequestsController do
-#   describe "GET '#index'" do
-#     it "returns http success" do
-#       get 'index'
-#       response.should be_success
-#     end
-#   end
+describe RequestsController do
+  describe 'GET #new' do
+    it 'returns HTTP success' do
+      get 'new'
+      response.should be_success
+    end
+  end
 
-#   describe "GET '#show'" do
-#     it "returns http success" do
-#       get 'show'
-#       response.should be_success
-#     end
-#   end
-# end
+  describe 'POST #create' do
+    it 'returns HTTP success' do
+      post_data = {
+        :rid             => SecureRandom.uuid,
+        :request_type    => 'GET',
+        :url             => 'http://google.com',
+        :private_request => 1,
+        :request_data => {
+          :headers => { 'X-Example' => 'Example value'},
+          :data    => {},
+        }.to_json,
+        :response_data => {
+          :headers => { 'X-Example' => 'Example value'},
+          :body    => 'Blah, body',
+          :status  => 200,
+        }.to_json
+      }
+
+      post 'create', post_data
+      response.should be_success
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
+end
