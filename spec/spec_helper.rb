45e4a8e1ebdd7f0169071cf19c740c0bef371dd5
Jacob Bednarz <jacob.bednarz@gmail.com>
2014-03-01 19:50:18 +1000

    Get some tests running :sparkles:

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.include Rails.application.routes.url_helpers
