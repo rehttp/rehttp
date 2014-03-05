f6b62ab818d1ac6a89feae32d70793e31725d8cc
Jacob Bednarz <jacob.bednarz@gmail.com>
2014-03-05 18:51:24 +1000

    Add some test coverage - Application wide.

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -1,11 +1,42 @@
 require 'spec_helper'
-require 'securerandom'
 
 describe RequestsController do
   describe 'GET #new' do
     it 'returns HTTP success' do
       get 'new'
       response.should be_success
+    end
+  end
+
+  describe 'POST #create' do
+    it 'returns HTTP success' do
+      request = build(:requests)
+      response.should be_success
+    end
+  end
+
+  describe 'GET #show_raw' do
+    context 'when request ID is available' do
+      it 'returns HTTP success' do
+        request = create(:requests)
+        get 'show_raw', :rid => request.rid
+
+        response.should be_success
+      end
+    end
+
+    context 'when request ID is not created' do
+      it 'throws a 404' do
+        get 'show_raw', :rid => create_uuid
+        
+        expect(response.status).to eq(404)
+      end
+    end
+
+    context 'when a request is private' do 
+      it 'appends correct no-cache headers' do
+        # check the cache control header
+      end
     end
   end
 end
--- spec/helpers/requests_helper_spec.rb
+++ spec/helpers/requests_helper_spec.rb
@@ -1,17 +1,31 @@
 require 'spec_helper'
 
 describe RequestsHelper do
-  it 'correctly formats HTTP headers' do
-    expect(format_http_header('x-cache-control')).to eq('X-Cache-Control')
+  describe '#format_http_header' do
+    it 'correctly formats HTTP headers' do
+      expect(format_http_header('x-cache-control')).to eq('X-Cache-Control')
+    end
   end
 
-  it 'returns true when JSON is in the header string' do
-    content_type_header = 'application/json; charset=utf-8'
-    expect(json_response?(content_type_header)).to be_true
+  describe '#json_response?' do
+    context 'when header contains JSON' do
+      it 'returns true' do
+        content_type_header = 'application/json; charset=utf-8'
+        expect(json_response?(content_type_header)).to be_true
+      end
+    end
+
+    context 'when header does not contain JSON' do
+      it 'returns false' do
+        content_type_header = 'text/html; charset=utf-8'
+        expect(json_response?(content_type_header)).to be_false
+      end
+    end
   end
 
-  it 'returns false when JSON is not in the header' do
-    content_type_header = 'text/html; charset=utf-8'
-    expect(json_response?(content_type_header)).to be_false
+  describe '#request_method_options' do
+    it 'should return as an array' do
+      expect(request_method_options).to be_instance_of(Array)
+    end
   end
 end
